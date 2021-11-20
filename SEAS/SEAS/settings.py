b936f5d60a90b0468b476a7edfd002eba996233e
Shafayat1004 <shafayat1004@live.com>
2021-11-21 02:44:15 +0600

    added comma to settings.py for installed apps

--- SEAS/SEAS/settings.py
+++ SEAS/SEAS/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
-    'database'
-    'login'
+    'home',
+    'database',
+    'login',
 ]
 
 MIDDLEWARE = [
