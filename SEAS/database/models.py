e3558b0e94b9255fde8d4c7e6955921b219ff0a3
Shafayat1004 <shafayat1004@live.com>
2021-11-21 03:00:49 +0600

    creating the database model

--- SEAS/database/models.py
+++ SEAS/database/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class FixedCharField( models.Field ):
+    def __init__( self, max_length, *args, **kwargs ):
+        self.max_length = max_length
+        super().__init__( *args, **kwargs )
+
+    def db_type( self, connection ):
+        return 'CHAR( %s )' % self.max_length
+
+class YearField( models.Field ):
+    def db_type( self, connection ):
+        return 'YEAR'
+
+class TimeField( models.Field ):
+    def db_type( self, connection ):
+        return 'TIME'
+
+class School ( models.Model ):
+    cSchool_ID = models.FixedCharField( max_length = 5 , primary_key = True )
+    cSchoolName = models.CharField( max_length = 50 )
