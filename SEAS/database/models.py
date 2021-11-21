ba167e2f214e194cb9b61b7261f60dbb8e2fb6f4
Fahim Ahmed <90824421+treeDweller98@users.noreply.github.com>
2021-11-21 12:33:49 +0600

    Update models.py

--- SEAS/database/models.py
+++ SEAS/database/models.py
@@ -20,19 +20,16 @@ class School ( models.Model ):
     cSchool_ID = FixedCharField(max_length = 5, primary_key = True)
     cSchoolName = models.CharField(max_length = 50)
 
-
 class Department ( models.Model ):
     cDepartment_ID = models.FixedCharField( max_length = 3, primary_key = True )
     cDepartmentName = models.CharField( max_length = 50 )
     cSchool_ID = models.ForeignKey( School, on_delete = models.CASCADE )
-
 
 class Course ( models.Model ):
     cCourse_ID = models.FixedCharField( max_length = 7, primary_key = True )
     cCourseName = models.CharField( max_length = 50 )
     nCreditHours = models.PositiveSmallIntegerField( max_length = 50 )
     cDepartment_ID = models.ForeignKey( Department, on_delete = models.CASCADE )
-
 
 class Faculty ( models.Model ):
     cFaculty_ID = models.FixedCharField( max_length = 4, primary_key = True )
