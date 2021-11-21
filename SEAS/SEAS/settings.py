d0720cbaaa2066d2972243464e5d498bf1006873
Shafayat1004 <shafayat1004@live.com>
2021-11-21 14:05:48 +0600

    added STATIC File settings

--- SEAS/SEAS/settings.py
+++ SEAS/SEAS/settings.py
@@ -128,6 +128,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    Path.joinpath(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
