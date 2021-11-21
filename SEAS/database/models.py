f7bb428c712e71baf368577684a5bac246c40ce5
Shafayat1004 <shafayat1004@live.com>
2021-11-21 14:38:38 +0600

    fixed an error where FixedCharField not in models.

--- SEAS/database/models.py
+++ SEAS/database/models.py
@@ -21,16 +21,16 @@ class School ( models.Model ):
     cSchoolName = models.CharField(max_length = 50)
 
 class Department ( models.Model ):
-    cDepartment_ID = models.FixedCharField( max_length = 3, primary_key = True )
+    cDepartment_ID = FixedCharField( max_length = 3, primary_key = True )
     cDepartmentName = models.CharField( max_length = 50 )
     cSchool_ID = models.ForeignKey( School, on_delete = models.CASCADE )
 
 class Course ( models.Model ):
-    cCourse_ID = models.FixedCharField( max_length = 7, primary_key = True )
+    cCourse_ID = FixedCharField( max_length = 7, primary_key = True )
     cCourseName = models.CharField( max_length = 50 )
-    nCreditHours = models.PositiveSmallIntegerField( max_length = 50 )
+    nCreditHours = models.PositiveSmallIntegerField()
     cDepartment_ID = models.ForeignKey( Department, on_delete = models.CASCADE )
 
 class Faculty ( models.Model ):
-    cFaculty_ID = models.FixedCharField( max_length = 4, primary_key = True )
+    cFaculty_ID = FixedCharField( max_length = 4, primary_key = True )
     cFacultyName = models.CharField( max_length = 50 )
