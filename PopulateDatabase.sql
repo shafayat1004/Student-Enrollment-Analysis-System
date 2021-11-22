d31fa388c0dc226ec5fd7b27f6878f00b3856261
Shafayat1004 <shafayat1004@live.com>
2021-11-23 01:13:52 +0600

    modified for supporting a optimised xlsx file.

---
+++ PopulateDatabase.sql
@@ -0,0 +1,73 @@
+-- AUTO-GENERATED QUERY FROM populateALLTables FUNCTION
+
+
+-- Populating Classroom_T
+LOAD DATA LOCAL
+INFILE "/home/shafayat/Coding/django/2021 Summer and Spring original.csv" 
+INTO TABLE Classroom_T 
+FIELDS TERMINATED BY ","
+IGNORE 1 LINES 
+(@d,@d,@d,@d,@d,@d,@d,@d,@ROOM_ID,@ROOM_CAPACITY,@d,@d,@d,@d,@d,@d,@d,@d) 
+SET cRoom_ID=@ROOM_ID,nRoomCapacity=@ROOM_CAPACITY;
+
+
+-- Populating Faculty_T
+LOAD DATA LOCAL
+INFILE "/home/shafayat/Coding/django/2021 Summer and Spring original.csv" 
+INTO TABLE Faculty_T 
+FIELDS TERMINATED BY ","
+IGNORE 1 LINES 
+(@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d) 
+SET cFaculty_ID=@FACULTY_ID,cFacultyName=@FACULTY_NAME;
+
+
+-- Populating School_T
+LOAD DATA LOCAL
+INFILE "/home/shafayat/Coding/django/2021 Summer and Spring original.csv" 
+INTO TABLE School_T 
+FIELDS TERMINATED BY ","
+IGNORE 1 LINES 
+(@SCHOOL_TITLE,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d) 
+SET cSchool_ID=@SCHOOL_TITLE,cSchoolName=@SCHOOL_NAME;
+
+
+-- Populating Department_T
+LOAD DATA LOCAL
+INFILE "/home/shafayat/Coding/django/2021 Summer and Spring original.csv" 
+INTO TABLE Department_T 
+FIELDS TERMINATED BY ","
+IGNORE 1 LINES 
+(@SCHOOL_TITLE,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d) 
+SET cDepartment_ID=@DEPARTMENT_ID,cDepartmentName=@DEPARTMENT_NAME,cSchool_ID=@SCHOOL_TITLE;
+
+
+-- Populating Course_T
+LOAD DATA LOCAL
+INFILE "/home/shafayat/Coding/django/2021 Summer and Spring original.csv" 
+INTO TABLE Course_T 
+FIELDS TERMINATED BY ","
+IGNORE 1 LINES 
+(@d,@COFFER_COURSE_ID,@d,@d,@CREDIT_HOUR,@d,@d,@d,@d,@d,@d,@COURSE_NAME,@d,@d,@d,@d,@d,@d) 
+SET cCourse_ID=@COFFER_COURSE_ID,cCourseName=@COURSE_NAME,nCreditHours=@CREDIT_HOUR;
+
+
+-- Populating CoOfferedCourse_T
+LOAD DATA LOCAL
+INFILE "/home/shafayat/Coding/django/2021 Summer and Spring original.csv" 
+INTO TABLE CoOfferedCourse_T 
+FIELDS TERMINATED BY ","
+IGNORE 1 LINES 
+(@d,@COFFER_COURSE_ID,@COFFERED_WITH,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d,@d) 
+SET cCoffCode_ID=@COFFERED_WITH,cCourse_ID=@COFFER_COURSE_ID;
+
+
+-- Populating Section_T
+LOAD DATA LOCAL
+INFILE "/home/shafayat/Coding/django/2021 Summer and Spring original.csv" 
+INTO TABLE Section_T 
+FIELDS TERMINATED BY ","
+IGNORE 1 LINES 
+(@d,@COFFER_COURSE_ID,@d,@SECTION,@d,@CAPACITY,@ENROLLED,@d,@ROOM_ID,@d,@BLOCKED,@d,@d,@d,@END_TIME,@ST_MW,@Year,@Semester) 
+SET eSession=@Semester,eDays=@ST_MW,dYear=@Year,nSectionNumber=@SECTION,nSectionCapacity=@CAPACITY,nEnrolled=@ENROLLED,bIsBlocked=@BLOCKED,tStartTime=@START_TIME,tEndTime=@END_TIME,cCoffCode_ID=@COFFER_COURSE_ID,cFaculty_ID=@FACULTY_ID,cRoom_ID=@ROOM_ID;
+
+
--- populationFunctions.py
+++ populationFunctions.py
@@ -2,7 +2,7 @@ import csv
 from pathlib import Path
 from pandas import read_excel, read_csv
 
-def csvToMySQL(csvPath, csvColumns, tableName, tableColumns):
+def csvToMySQL(csvPath, csvColumns, tableName, tableColumns, local=True): #TODO might have to set this to false for the actual app
     csv_variables = ''
 
     firstRow = read_csv(csvPath, nrows=1)
@@ -28,7 +28,11 @@ def csvToMySQL(csvPath, csvColumns, tableName, tableColumns):
             tableAssignments+=',' + tableColumns[i] + '=@' + csvColumns[i]
 
 
-    sqlQuery = 'LOAD DATA \nINFILE "' + str(csvPath) + '" \nINTO TABLE ' + tableName + ' \nFIELDS TERMINATED BY ","\nIGNORE 1 LINES \n' + csv_variables + ' \nSET ' + tableAssignments + ';'
+    sqlQuery = 'LOAD DATA '
+    if local is True:
+        sqlQuery+= 'LOCAL'
+
+    sqlQuery+='\nINFILE "' + str(csvPath) + '" \nINTO TABLE ' + tableName + ' \nFIELDS TERMINATED BY ","\nIGNORE 1 LINES \n' + csv_variables + ' \nSET ' + tableAssignments + ';'
     
     return sqlQuery
 
@@ -40,10 +44,82 @@ def xlsxToMySQL(xlsxPath, xlsxColumns, tableName, tableColumns):
     read_file.to_csv (csvSavePath, index = None, header=True, columns=xlsxColumns)
     return csvToMySQL(csvSavePath, xlsxColumns, tableName, tableColumns)
 
+def fillClassroom_T(csvPath):
+    table = 'Classroom_T'
+    csvColumns = ['ROOM_ID', 'ROOM_CAPACITY']
+    tableColumns = ['cRoom_ID', 'nRoomCapacity']
+    return '-- Populating ' + table + '\n' + csvToMySQL(csvPath, csvColumns, table, tableColumns) + '\n\n\n'
+
+def fillFaculty_T(csvPath):
+    table = 'Faculty_T'
+    # TODO need to separate FACULTY ID and NAME
+
+    csvColumns = ['FACULTY_ID', 'FACULTY_NAME']
+    tableColumns = ['cFaculty_ID', 'cFacultyName']
+    return '-- Populating ' + table + '\n' + csvToMySQL(csvPath, csvColumns, table, tableColumns) + '\n\n\n'
+
+def fillSchool_T(csvPath):
+    table = 'School_T'
+    #TODO  There is no such thing as schoolname in any dataset
+
+    csvColumns = ['SCHOOL_TITLE', 'SCHOOL_NAME']
+    tableColumns = ['cSchool_ID', 'cSchoolName']
+    return '-- Populating ' + table + '\n' + csvToMySQL(csvPath, csvColumns, table, tableColumns) + '\n\n\n'
+
+def fillDepartment_T(csvPath):
+    #TODO  There is no such thing as department id in classsize dataset
+
+    table = 'Department_T'
+    csvColumns = ['DEPARTMENT_ID', 'DEPARTMENT_NAME', 'SCHOOL_TITLE']
+    tableColumns = ['cDepartment_ID', 'cDepartmentName', 'cSchool_ID']
+    return '-- Populating ' + table + '\n' + csvToMySQL(csvPath, csvColumns, table, tableColumns) + '\n\n\n'
+
+def fillCourse_T(csvPath):
+    table = 'Course_T'
+    csvColumns = ['COFFER_COURSE_ID', 'COURSE_NAME', 'CREDIT_HOUR']
+    tableColumns = ['cCourse_ID', 'cCourseName', 'nCreditHours'] 
+    #TODO  There is no such thing as department id in classsize dataset so omitted mentioning 'cDepartment_ID'] 
+    
+    return '-- Populating ' + table + '\n' + csvToMySQL(csvPath, csvColumns, table, tableColumns) + '\n\n\n'
+
+def fillCoOfferedCourse_T(csvPath):
+    table = 'CoOfferedCourse_T'
+    csvColumns = ['COFFERED_WITH', 'COFFER_COURSE_ID']
+    # TODO need to separate multivalued COFFERED_WITH ids
+
+    tableColumns = ['cCoffCode_ID', 'cCourse_ID']
+    return '-- Populating ' + table + '\n' + csvToMySQL(csvPath, csvColumns, table, tableColumns) + '\n\n\n'
+
+def fillSection_T(csvPath):
+    table = 'Section_T'
+    csvColumns = ['Semester', 'ST_MW', 'Year', 'SECTION', 'CAPACITY', 'ENROLLED', 'BLOCKED', 'START_TIME', 'END_TIME', 'COFFER_COURSE_ID', 'FACULTY_ID', 'ROOM_ID'] 
+    #Took liberty here with start time. In dataset it's STRAT_TIME 
+    # TODO need to separate FACULTY ID and NAME
+
+    tableColumns = ['eSession', 'eDays', 'dYear', 'nSectionNumber', 'nSectionCapacity', 'nEnrolled', 'bIsBlocked', 'tStartTime', 'tEndTime', 'cCoffCode_ID', 'cFaculty_ID', 'cRoom_ID']
+    return '-- Populating ' + table + '\n' + csvToMySQL(csvPath, csvColumns, table, tableColumns) + '\n\n\n'
+
+def populateAllTables(xlsxPath):
+    read_file = read_excel (xlsxPath)
+    csvSavePath = Path.joinpath(xlsxPath.parent, xlsxPath.stem + '.csv')
+    read_file.to_csv (csvSavePath, index = None, header=True)
+
+    return '-- AUTO-GENERATED QUERY FROM populateALLTables FUNCTION\n\n\n' + fillClassroom_T(csvSavePath) + fillFaculty_T(csvSavePath) + fillSchool_T(csvSavePath) + fillDepartment_T(csvSavePath) + fillCourse_T(csvSavePath) + fillCoOfferedCourse_T(csvSavePath) + fillSection_T(csvSavePath)
+
+
+def optimiseXLSX(xlsxPath):
+    # TODO We will have to fix the problems outlined in the previous functions then send to populate Function
+    return
+
 
 xlsxPath = Path('/home/shafayat/Coding/django/2021 Summer and Spring original.xlsx')
-xlsxColumns = ['ROOM_ID', 'ROOM_CAPACITY']
-tableColumns = ['cRoom_ID', 'nRoomCapacity']
-tableName = 'TESTSite_Database.Classroom_T'
 
-print(xlsxToMySQL(xlsxPath, xlsxColumns, tableName, tableColumns))
+# xlsxColumns = ['ROOM_ID', 'ROOM_CAPACITY']
+# tableColumns = ['cRoom_ID', 'nRoomCapacity']
+# tableName = 'TESTSite_Database.Classroom_T'
+
+output = populateAllTables(xlsxPath)
+print(output)
+with open("PopulateDatabase.sql", "w") as text_file:
+    text_file.write(output)
+
