b58286072b96b23d1b817397749ecaa96c99a160
treeDweller98 <personal.fahim00@gmail.com>
2021-12-10 15:51:21 +0600

    added chart data packaging utilities

--- SEAS/revenue/utils.py
+++ SEAS/revenue/utils.py
@@ -1,10 +1,11 @@
 # util/charts.py
-colorPalette = ['#55efc4', '#81ecec', '#a29bfe', '#ffeaa7', '#fab1a0', '#ff7675', '#fd79a8']
+colorPalette = ['#55efc4', '#81ecec', '#a29bfe', '#ffeaa7', '#fab1a0', '#ff7675', '#fd79a8',  '#9b59b6',
+                   '#d68910', '#196f3d', '#85c1e9', '#34495e', '#2c3e50' ]
 colorPrimary, colorSuccess, colorDanger = '#79aec8', colorPalette[0], colorPalette[5]
 
 def generate_color_palette( amount ):
-    palette = []
 
+    palette = []
     i = 0
     while i < len(colorPalette) and len(palette) < amount:
         palette.append(colorPalette[i])
@@ -13,3 +14,30 @@ def generate_color_palette( amount ):
             i = 0
 
     return palette
+
+
+
+def revenueChartDataPacker( dataTable, colNames, isArea = False ):
+    ''' Packages rows and their column labels into an easy-to-chart form.         
+        "dataTable" is a list of tuples, where each tuple is a row of the table,         
+        "colNames" is the column names of the table, "isArea" = false is for line chart selector '''
+
+    columns = [ zip( *dataTable ) ]                              # unzips tuples of rows, and aligns corresponding elements to form tuples of cols
+
+    colors = generate_color_palette( len( columns ) )            # unique color list for every column
+
+    xAxisValues = columns[0]                                     # first element of each row tuple is semester alt: [ row[0] for row in dataTable ]
+
+    print( xAxisValues )
+    
+    yAxisValues = [                                              
+        { 'labels': label, 'data': columns[i+1], 'borderColor': colors[i+1],  'fill': isArea }
+        for i, label in enumerate( colNames[ 1 : ] )
+    ]  # list of dictionaries containing e.g. { label: "SETS", data: [134,67,67..], borderColor:'#55efc4', fill: true }
+
+    return xAxisValues, yAxisValues         
+    
+    
+    
+            
+
--- SEAS/revenue/views.py
+++ SEAS/revenue/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.db import connection
 from django.http import HttpResponse
 
+from SEAS.revenue.utils import revenueChartDataPacker
+
 # Fahim's stuff
 # path( 'revenue/', include('revenue.urls') ),
 
@@ -64,10 +66,14 @@ def iubRevenue( request ):
         cursor.execute( query )
         labels = [ col[0] for col in cursor.description ]
         data = cursor.fetchall()
+
+    xAxis, yAxis = revenueChartDataPacker( data, labels )
         
     return render( request, "revenue_iub.html", { 
             'colNames': labels,
             'revenues': data,
+            'xAxis': xAxis,
+            'yAxis': yAxis 
         }
     )
 
