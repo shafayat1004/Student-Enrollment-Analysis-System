c814dc91fb6f4970f0f9325b7b812d863dea0546
Sue200 <msuhaila47@gmail.com>
2021-12-13 13:12:14 +0600

    Created IUB-resource Table + query

--- SEAS/resources/views.py
+++ SEAS/resources/views.py
@@ -112,6 +112,76 @@ def resourceUsage(request):
 
 def iubResources(request):
 
-    return render(request, 'resources/iub_resources.html')
+    tableHeaders = []
+    tableData = []
+
+    query = """
+        SELECT 
+		    Class_Size,
+            Counter AS "IUB Resources",
+            (Class_Size*Counter) AS "Capacity"
+        FROM (
+		    SELECT ( 
+			    CASE 
+                    WHEN C.nRoomCapacity = 20 THEN '20'  
+                    WHEN C.nRoomCapacity = 30 THEN '30' 
+                    WHEN C.nRoomCapacity = 40 THEN '40'  
+                    WHEN C.nRoomCapacity = 50 THEN '50'   
+                    WHEN C.nRoomCapacity = 54 THEN '54' 
+                    WHEN C.nRoomCapacity = 64 THEN '64' 
+                    WHEN C.nRoomCapacity = 124 THEN '124'  
+                    WHEN C.nRoomCapacity = 168 THEN '168'
+                END 
+		    ) AS Class_Size, COUNT(*) AS Counter
+		    FROM  Classroom_T AS C
+		    GROUP BY Class_Size -- ,C.nRoomCapacity
+            HAVING Class_Size >=20
+		    ORDER BY Class_Size ASC
+        
+        ) E
+        GROUP BY Class_Size
+        -- ORDER BY Class_Size
+
+        UNION
+        SELECT 
+	        "Total",
+            SUM(Counter),
+            SUM(Class_Size*Counter)
+        FROM (
+            SELECT ( 
+                CASE 
+                    WHEN C.nRoomCapacity = 20 THEN '20'  
+                    WHEN C.nRoomCapacity = 30 THEN '30' 
+                    WHEN C.nRoomCapacity = 40 THEN '40'  
+                    WHEN C.nRoomCapacity = 50 THEN '50'   
+                    WHEN C.nRoomCapacity = 54 THEN '54' 
+                    WHEN C.nRoomCapacity = 64 THEN '64' 
+                    WHEN C.nRoomCapacity = 124 THEN '124'  
+                    WHEN C.nRoomCapacity = 168 THEN '168'
+                END 
+		    ) AS Class_Size, COUNT(*) AS Counter
+		    FROM  Classroom_T AS C
+		    GROUP BY Class_Size,C.nRoomCapacity
+            HAVING Class_Size >=20
+		    ORDER BY Class_Size ASC
+        ) T
+
+        """
+    
+    with connection.cursor() as cursor:
+        cursor.execute( query)
+        # print(connection.queries)
+        tableHeaders = [ col[0] for col in cursor.description ]
+        tableData = cursor.fetchall()   
+
+
+    context = {
+        'tableHeaders': tableHeaders,
+        'tableData'   : tableData,
+    }
+
+    return render(request, 'resources/iub_resources.html', context)
+
 def resourceComp(request):
+   
     return render(request, 'resources/resource_comparison.html')
