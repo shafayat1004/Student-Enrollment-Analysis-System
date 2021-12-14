66a693034b7b711196d034cfd4560387162a2bb8
Shafayat1004 <shafayat1004@live.com>
2021-12-14 20:01:37 +0600

    home page draft made

--- SEAS/home/views.py
+++ SEAS/home/views.py
@@ -1,4 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the home index.")
+    context = {
+
+    }
+    render(request, 'home/home.html')
